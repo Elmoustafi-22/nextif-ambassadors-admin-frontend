e3861871688ce0298e7c6b9f516940c0873ee076
Mustopha Abdulqadir <mustophaqadir@gmail.com>
2026-06-01 14:29:13 +0100

    feat: implement admin login and forgot password pages with routing support

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 import ProtectedRoute from "../components/ProtectedRoute";
 
 import LoginPage from "../pages/LoginPage";
+import ForgotPasswordPage from "../pages/ForgotPasswordPage";
 import ResetPasswordPage from "../pages/ResetPasswordPage";
 import AdminDashboard from "../pages/AdminDashboard";
 import AmbassadorListPage from "../pages/AmbassadorListPage";
@@ -30,6 +31,7 @@ const AppRoutes = () => {
     <Routes>
       {/* Public Routes */}
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/forgot-password" element={<ForgotPasswordPage />} />
       <Route path="/unauthorized" element={<Unauthorized />} />
 
       {/* Protected Routes - Admin Only */}
